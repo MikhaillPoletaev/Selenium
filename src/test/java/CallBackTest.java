cbc834dfa0752229044aa753d6512429c432a59d
Mikhail <mpolet@outlook.com>
2023-04-12 17:56:07 +0300

    Add code to CallBackTest

--- src/test/java/CallBackTest.java
+++ src/test/java/CallBackTest.java
@@ -1,2 +1,23 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class CallBackTest {
-}
+        private WebDriver driver;
+        @BeforeEach
+        void setUp() {
+            driver = new ChromeDriver();
+        }
+        @AfterEach
+        void tearDown() {
+            driver.quit();
+            driver = null;
+        }
+        @Test
+        void shouldTestSomething() {
+            throw new UnsupportedOperationException();
+        }
+
+    }
