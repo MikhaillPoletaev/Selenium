71b78b27f827a46dc9a07e66e730398cb830f787
Mikhail <mpolet@outlook.com>
2023-04-13 16:38:30 +0300

    Update 1

--- src/test/java/CallBackTest.java
+++ src/test/java/CallBackTest.java
@@ -1,14 +1,35 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import com.google.errorprone.annotations.Var;
+import org.checkerframework.checker.units.qual.C;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallBackTest {
         private WebDriver driver;
+        @BeforeAll
+        static void setUpAll() {
+            System.setProperty("webdriver.chrome.driver", "C:\\Users\\1\\IdeaProjects\\Selenium\\Drivers\\chromedriver.exe");
+        }
+
         @BeforeEach
         void setUp() {
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--remote-allow-origins=*");
+         //   options.addArguments("--headless");
+            driver = new ChromeDriver(options);
+            driver.get("http://localhost:9999/");
         }
         @AfterEach
         void tearDown() {
@@ -17,7 +38,13 @@ public class CallBackTest {
         }
         @Test
         void shouldTestSomething() {
-            throw new UnsupportedOperationException();
+            driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Гагарин Юрий");
+            driver.findElement(By.cssSelector("[data-test-id=phone] input" )).sendKeys("+797775558822");
+            driver.findElement(By.className("checkbox__box")).click();
+            driver.findElement(By.cssSelector("button.button")).click();
+            var actualText = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+            assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
+
         }
 
     }
